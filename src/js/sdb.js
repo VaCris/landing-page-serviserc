d4d359f95dc0aad8f8026d192edd8c09ed6d46e2
Vidal <antoniovidal2031@gmail.com>
2024-09-30 10:42:01 -0500

    v1.0.2

--- src/js/sdb.js
+++ src/js/sdb.js
@@ -1,26 +1,58 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.getElementById('menu-toggle');
     const sidebar = document.getElementById('sidebar');
-    const closeSidebar = document.getElementById('close-sidebar');
-    const menuLinks = document.querySelectorAll('#sidebar a');
+    const menuIcon = document.getElementById('menu-icon');
 
-    const toggleSidebar = () => {
-        const isOpen = sidebar.classList.toggle('open');
-        document.body.classList.toggle('menu-open', isOpen);
-        menuToggle.classList.toggle('hidden', isOpen);
-    };
+    if (!menuToggle || !sidebar || !menuIcon) {
+        console.error("Uno o más elementos no se encontraron.");
+        return;
+    }
+
+    let isOpen = false;
+
+    const menuAnimation = lottie.loadAnimation({
+        container: menuIcon,
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        path: '/public/img/Menu V4/menuV4.json'
+    });
+
+    menuToggle.addEventListener('click', () => {
+        if (isOpen) {
+            menuAnimation.setDirection(-1); 
+            menuAnimation.play();
+            sidebar.classList.remove('translate-y-0');
+            sidebar.classList.add('-translate-y-full');
+            menuToggle.classList.remove('hidden');
+        } else {
+            sidebar.classList.remove('-translate-y-full');
+            sidebar.classList.add('translate-y-0');
+            menuAnimation.setDirection(1);
+            menuAnimation.play();
+            menuToggle.classList.add('hidden');
+        }
+        isOpen = !isOpen;
+    });
+
     const closeSidebarMenu = () => {
-        sidebar.classList.remove('open');
-        document.body.classList.remove('menu-open');
-        menuToggle.classList.remove('hidden');
+        if (isOpen) {
+            menuAnimation.setDirection(-1);
+            menuAnimation.play();
+            sidebar.classList.remove('translate-y-0');
+            sidebar.classList.add('-translate-y-full');
+            menuToggle.classList.add('hidden');
+            isOpen = false;
+        }
     };
-    menuToggle.addEventListener('click', toggleSidebar);
-    closeSidebar.addEventListener('click', closeSidebarMenu);
+
+    const menuLinks = document.querySelectorAll('#sidebar a');
     menuLinks.forEach(link => {
         link.addEventListener('click', closeSidebarMenu);
     });
+    
     document.addEventListener('click', event => {
-        if (!sidebar.contains(event.target) && !menuToggle.contains(event.target)) {
+        if (!sidebar.contains(event.target) && !menuToggle.contains(event.target) && isOpen) {
             closeSidebarMenu();
         }
     });
