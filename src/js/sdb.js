58a58d1553bef7bbe209bf494da69ffa378fd00c
oficina.vidal <correo_test2@outlook.com>
2024-10-16 13:00:27 -0500

    V1.0.3

--- src/js/sdb.js
+++ src/js/sdb.js
@@ -1,17 +1,27 @@
 document.getElementById('menu-button').addEventListener('click', function () {
+  const menuIcon = document.getElementById('menu-icon');
+  const closeIcon = document.getElementById('close-icon');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  if (!mobileMenu.classList.contains('open')) {
+    mobileMenu.classList.add('open'); // Muestra el menú
+    menuIcon.classList.add('hidden');  // Oculta el ícono de menú
+    closeIcon.classList.remove('hidden'); // Muestra el ícono de cerrar
+  } else {
+    mobileMenu.classList.remove('open'); // Oculta el menú
+    menuIcon.classList.remove('hidden');  // Muestra el ícono de menú
+    closeIcon.classList.add('hidden');     // Oculta el ícono de cerrar
+  }
+});
+
+document.querySelectorAll('#mobile-menu a').forEach(item => {
+  item.addEventListener('click', function () {
+    const mobileMenu = document.getElementById('mobile-menu');
     const menuIcon = document.getElementById('menu-icon');
     const closeIcon = document.getElementById('close-icon');
-    const mobileMenu = document.getElementById('mobile-menu');
 
-    if (mobileMenu.classList.contains('hidden')) {
-        mobileMenu.classList.remove('hidden');
-        mobileMenu.classList.add('open');
-        menuIcon.classList.add('open');
-        closeIcon.classList.add('open');
-    } else {
-        mobileMenu.classList.add('hidden');
-        mobileMenu.classList.remove('open');
-        menuIcon.classList.remove('open');
-        closeIcon.classList.remove('open');
-    }
+    mobileMenu.classList.remove('open'); // Oculta el menú
+    menuIcon.classList.remove('hidden');  // Muestra el ícono de menú
+    closeIcon.classList.add('hidden');     // Oculta el ícono de cerrar
+  });
 });
