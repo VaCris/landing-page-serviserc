d75f6aade2498736f680711dbca26b7b1c9b28d3
user.oficina <correo_test2@outlook.com>
2024-10-16 10:03:20 -0500

    V1.0.3

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,20 @@ module.exports = {
       './src/js/**/*.js',
   ],
   theme: {
-      extend: {},
+      extend: {
+          screens: {
+              'xxs': '320px',   
+              'xs': '480px',    
+              'sm': '640px',    
+              'md': '768px',    
+              'lg': '1024px',  
+              'xl': '1280px',   
+              '2xl': '1536px',  
+              '3xl': '1600px',  
+              '4xl': '1920px',  
+              '5xl': '2560px',  
+          },
+      },
   },
   plugins: [],
 };
