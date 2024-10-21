699120fb89398e245a5f01c54585f1f6c7971b53
oficina.vidal <correo_test2@outlook.com>
2024-10-21 12:42:58 -0500

    V1.0.3

--- src/js/sdb.js
+++ src/js/sdb.js
@@ -1,40 +1,12 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const menuButton = document.getElementById('menu-button');
-  const menuIcon = document.getElementById('menu-icon');
-  const closeIcon = document.getElementById('close-icon');
-  const mobileMenu = document.getElementById('mobile-menu');
-  const contentDiv = document.querySelector('.mt-12');
-  const pagination = document.querySelector('.pagination');
-  const whatsappButton = document.querySelector('.nav-bottom');
+const menuButton = document.getElementById('menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+const menuIcon = document.getElementById('menu-icon');
+const closeIcon = document.getElementById('close-icon');
 
-  menuButton.addEventListener('click', function () {
-    const isOpen = mobileMenu.classList.contains('open');
-
-    if (!isOpen) {
-      mobileMenu.classList.add('open');
-      menuIcon.classList.add('hidden');
-      closeIcon.classList.remove('hidden');
-      contentDiv.classList.add('hidden-content');
-      pagination.classList.add('hidden-content');
-      whatsappButton.classList.add('hidden-content');
-    } else {
-      mobileMenu.classList.remove('open');
-      menuIcon.classList.remove('hidden');
-      closeIcon.classList.add('hidden');
-      contentDiv.classList.remove('hidden-content');
-      pagination.classList.remove('hidden-content');
-      whatsappButton.classList.remove('hidden-content');
-    }
-  });
-
-  document.querySelectorAll('#mobile-menu a').forEach(item => {
-    item.addEventListener('click', function () {
-      mobileMenu.classList.remove('open');
-      menuIcon.classList.remove('hidden');
-      closeIcon.classList.add('hidden');
-      contentDiv.classList.remove('hidden-content');
-      pagination.classList.remove('hidden-content');
-      whatsappButton.classList.remove('hidden-content');
-    });
-  });
+menuButton.addEventListener('click', () => {
+    const isOpen = mobileMenu.classList.toggle('open');
+    menuButton.setAttribute('aria-expanded', isOpen);
+    closeIcon.classList.toggle('hidden', !isOpen);
+    menuIcon.classList.toggle('hidden', isOpen);
+    document.body.classList.toggle('menu-open', isOpen);
 });
