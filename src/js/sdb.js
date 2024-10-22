6e17ed801e90ff3511c6b98c091bed1d00a562b4
Vidal <antoniovidal2031@gmail.com>
2024-10-21 23:28:06 -0500

    V1.0.3

--- src/js/sdb.js
+++ src/js/sdb.js
@@ -10,3 +10,18 @@ menuButton.addEventListener('click', () => {
     menuIcon.classList.toggle('hidden', isOpen);
     document.body.classList.toggle('menu-open', isOpen);
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.getElementById("menu-button");
+    const mobileMenu = document.getElementById("mobile-menu");
+    const menuIcon = document.getElementById("menu-icon");
+    const closeIcon = document.getElementById("close-icon");
+  
+    menuButton.addEventListener("click", () => {
+      const isMenuOpen = mobileMenu.classList.toggle("-translate-y-full");
+  
+      // Alternar iconos
+      menuIcon.classList.toggle("hidden", isMenuOpen);  // Ocultar menú hamburguesa si el menú está abierto
+      closeIcon.classList.toggle("hidden", !isMenuOpen);  // Mostrar icono de cerrar si el menú está abierto
+    });
+  });
+  
