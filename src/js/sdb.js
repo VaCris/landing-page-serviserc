86bde95066b553f4bb7e5d153f64706c9d00b00c
user.oficina <correo_test2@outlook.com>
2024-10-28 12:48:03 -0500

    V1.0.4

--- src/js/sdb.js
+++ src/js/sdb.js
@@ -1,26 +1,41 @@
-const menuButton = document.getElementById('menu-button');
-const mobileMenu = document.getElementById('mobile-menu');
-const menuIcon = document.getElementById('menu-icon');
-const closeIcon = document.getElementById('close-icon');
+document.addEventListener('DOMContentLoaded', function () {
+    const menubtn = document.getElementById('menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+    const menuIcon = document.getElementById('menu-icon');
+    const closeIcon = document.getElementById('close-icon');
+    const wspbtn = document.querySelector('.nav-bottom');
 
-menuButton.addEventListener('click', () => {
-    const isOpen = mobileMenu.classList.toggle('open');
-    menuButton.setAttribute('aria-expanded', isOpen);
-    closeIcon.classList.toggle('hidden', !isOpen);
-    menuIcon.classList.toggle('hidden', isOpen);
-    document.body.classList.toggle('menu-open', isOpen);
+    function toggleMenu() {
+        const isOpen = mobileMenu.classList.toggle('hidden');
+        console.log(`Menu esta ${mobileMenu.classList.contains('hidden') ? 'Oculto' : 'Visibe'}`);
+
+        menubtn.setAttribute('aria-expanded', !isOpen);
+        menuIcon.classList.toggle('hidden', !isOpen);
+        closeIcon.classList.toggle('hidden', isOpen);
+        console.log(`Menu Icon: ${menuIcon.classList.contains('hidden') ? 'Oculto' : 'Visible'}`);
+        console.log(`Close Icon: ${closeIcon.classList.contains('hidden') ? 'Oculto' : 'Visible'}`);
+        console.log(`Close Icon estilos: `, window.getComputedStyle(closeIcon));
+
+        if (wspbtn) {
+            wspbtn.classList.toggle('hidden', !isOpen);
+        }
+    }
+
+    if (menubtn) {
+        menubtn.addEventListener('click', toggleMenu);
+    } else {
+        console.error('btn menu no encontrado.');
+    }
+
+    if (mobileMenu) {
+        mobileMenu.querySelectorAll('a').forEach(link => {
+            link.addEventListener('click', () => {
+                if (!mobileMenu.classList.contains('hidden')) {
+                    toggleMenu();
+                }
+            });
+        });
+    } else {
+        console.error('menu movil no encontrado.');
+    }
 });
-document.addEventListener("DOMContentLoaded", function () {
-    const menuButton = document.getElementById("menu-button");
-    const mobileMenu = document.getElementById("mobile-menu");
-    const menuIcon = document.getElementById("menu-icon");
-    const closeIcon = document.getElementById("close-icon");
-  
-    menuButton.addEventListener("click", () => {
-      const isMenuOpen = mobileMenu.classList.toggle("-translate-y-full");
-  
-      menuIcon.classList.toggle("hidden", isMenuOpen);  
-      closeIcon.classList.toggle("hidden", !isMenuOpen); 
-    });
-  });
-  
