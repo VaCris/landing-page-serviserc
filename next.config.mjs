39f46b6b9d03bfded8fd8ed4f372fd4573fc2f72
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-06-04 16:14:48 -0500

    chore: add next static export config

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+const repoName = 'landing-page-serviserc';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubPages ? `/${repoName}` : '',
+  assetPrefix: isGithubPages ? `/${repoName}/` : '',
+};
+
+export default nextConfig;
